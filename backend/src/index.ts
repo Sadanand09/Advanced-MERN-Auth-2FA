1c512512d25ccfef972c2686f0affdd47c4556f4
Sadanand09 <sadanandgurude9@gmail.com>
2025-12-30 19:33:16 +0530

    DB setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import express, { Request, Response} from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { config } from "./config/app.config";
+import connectDatabase from "./database/database";
 
 const app = express();
 //const BASE_PATH = config.BASE_PATH;
@@ -25,5 +26,6 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.listen(config.PORT, async () => {
-  console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
+    console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
+    await connectDatabase();
 });
