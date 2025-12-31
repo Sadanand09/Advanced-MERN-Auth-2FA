fcfd6557645b31d77c02600f46d81c6c34f4af53
Sadanand09 <sadanandgurude9@gmail.com>
2025-12-31 21:28:44 +0530

    Implement error handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
 import "dotenv/config";
-import express, { Request, Response} from "express";
+import express, { Request, Response, NextFunction} from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { config } from "./config/app.config";
 import connectDatabase from "./database/database";
+import { errorHandler } from "./middlewares/errorHandlers";
+import { HTTPSTATUS } from "./config/http.config";
+import { asyncHandler } from "./middlewares/asyncHandler";
 
 const app = express();
 //const BASE_PATH = config.BASE_PATH;
@@ -19,13 +22,19 @@ app.use(
 
 app.use(cookieParser());
 
-app.get("/", (req: Request, res: Response) => {
-    res.status(200).json({
-        message: "Hello subscribers!!!",
+app.get(
+  "/",
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    res.status(HTTPSTATUS.OK).json({
+      message: "Hello Subscribers!!!",
     });
-});
+  })
+);
+app.use(errorHandler);
 
 app.listen(config.PORT, async () => {
     console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
     await connectDatabase();
 });
+
+
