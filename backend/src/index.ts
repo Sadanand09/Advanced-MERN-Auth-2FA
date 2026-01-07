7ea603c5dc318396c3e61de648ba096a424750c6
Sadanand09 <sadanandgurude9@gmail.com>
2026-01-07 22:57:24 +0530

    register api & models

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,9 +7,10 @@ import connectDatabase from "./database/database";
 import { errorHandler } from "./middlewares/errorHandlers";
 import { HTTPSTATUS } from "./config/http.config";
 import { asyncHandler } from "./middlewares/asyncHandler";
+import authRoutes from "./modules/auth/auth.routes";
 
 const app = express();
-//const BASE_PATH = config.BASE_PATH;
+const BASE_PATH = config.BASE_PATH;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -30,6 +31,8 @@ app.get(
     });
   })
 );
+
+app.use(`${BASE_PATH}/auth`, authRoutes)
 app.use(errorHandler);
 
 app.listen(config.PORT, async () => {
