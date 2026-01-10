ab1c7d9bb90877143be27c4cad9860f53e29f68b
Sadanand09 <sadanandgurude9@gmail.com>
2026-01-10 23:13:18 +0530

    Backend completed

--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 import { config } from "../config/app.config";
 
 const connectDatabase = async () => {
-    try {
-        await mongoose.connect(config.MONGO_URI);
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(config.MONGO_URI);
+    console.log("Connected to Mongo database");
+  } catch (error) {
+    console.log("Error connecting to Mongo database");
+    process.exit(1);
+  }
+};
 
 export default connectDatabase;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,17 @@
 import "dotenv/config";
-import express, { Request, Response, NextFunction} from "express";
 import cors from "cors";
+import express, { NextFunction, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import { config } from "./config/app.config";
 import connectDatabase from "./database/database";
-import { errorHandler } from "./middlewares/errorHandlers";
+import { errorHandler } from "./middlewares/errorHandler";
 import { HTTPSTATUS } from "./config/http.config";
 import { asyncHandler } from "./middlewares/asyncHandler";
 import authRoutes from "./modules/auth/auth.routes";
+import passport from "./middlewares/passport";
+import sessionRoutes from "./modules/session/session.routes";
+import { authenticateJWT } from "./common/strategies/jwt.strategy";
+import mfaRoutes from "./modules/mfa/mfa.routes";
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -15,13 +19,14 @@ const BASE_PATH = config.BASE_PATH;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
-    cors({
-        origin: config.APP_ORIGIN,
-        credentials: true,
-    })
+  cors({
+    origin: config.APP_ORIGIN,
+    credentials: true,
+  })
 );
 
 app.use(cookieParser());
+app.use(passport.initialize());
 
 app.get(
   "/",
@@ -32,12 +37,15 @@ app.get(
   })
 );
 
-app.use(`${BASE_PATH}/auth`, authRoutes)
+app.use(`${BASE_PATH}/auth`, authRoutes);
+
+app.use(`${BASE_PATH}/mfa`, mfaRoutes);
+
+app.use(`${BASE_PATH}/session`, authenticateJWT, sessionRoutes);
+
 app.use(errorHandler);
 
 app.listen(config.PORT, async () => {
-    console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
-    await connectDatabase();
+  console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
+  await connectDatabase();
 });
-
-
